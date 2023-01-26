a8c81e7948e620a2562112aed8fc185c0a54bb81
ANKA-TR <67321675+ANKA-TR@users.noreply.github.com>
2023-01-26 11:17:30 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const { Client, Partials } = require('discord.js')
+const client = new Client({ intents: 131071, partials: Object.values(Partials).filter(x => typeof x === "string"), shards: 'auto' })
+const { botid, token } = require("./ayarlar.json")
+require("./slash")(client)
+
+// * ETİKETE TEPKİ
+client.on('messageCreate', async message => {
+
+    const embedetiket = new Discord.EmbedBuilder()
+    .setColor('Blue')
+    .setDescription(`**/yardım** Komutu ile Komutlarımı Görebilirsiniz`)
+    if (message.content === `<@${botid}>`) {
+      message.channel.send({ embeds: [embedetiket] })
+    }
+
+})
+
+client.on('guildMemberAdd', member => {
+  member.send("Hoş geldin").catch(err => console.log(err));
+})
+
+// ! TOKEN
+client.login(token)
